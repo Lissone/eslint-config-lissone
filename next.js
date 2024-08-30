5c467848f11c609c11fed537c4ad1cd7a40951a1
Lissone <leonardo.lissonez@gmail.com>
2024-08-29 23:51:11 -0300

    feat(next): add next plugin

--- next.js
+++ next.js
@@ -5,6 +5,8 @@ module.exports = {
     jest: true,
   },
   extends: [
+    'next',
+    'next/core-web-vitals',
     'eslint:recommended',
     'plugin:eslint-comments/recommended',
     'plugin:import/recommended',
